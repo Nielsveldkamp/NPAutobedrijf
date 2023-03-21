64e46fda8063d247796b61a3c733723cd5497f7e
Nielsveldkamp <Nveldkamp1@st.noorderpoort.nl>
2023-03-21 09:21:28 +0100

    added create

--- resources/views/auto/create.blade.php
+++ resources/views/auto/create.blade.php
@@ -65,11 +65,7 @@
                     </script>
 <x-guest-layout>
     <x-app-layout>
-        <x-slot name="header">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('auto') }}
-            </h2>
-        </x-slot>
+
         <x-form-card>
 
 
