1ec88af50617d2082d7526e4344e325d941667de
Nielsveldkamp <Nveldkamp1@st.noorderpoort.nl>
2023-03-29 09:41:45 +0200

    form css fix

--- resources/views/components/Form-card.blade.php
+++ resources/views/components/Form-card.blade.php
@@ -1,6 +1,5 @@
-
 <div class="min-h-eightyPscreen flex flex-col sm:justify-center items-center pt-6">
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <div class="w-full sm:max-w-md lg:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
         {{ $slot }}
     </div>
 </div>
