0b0aba9a5356db10df1c6f130730fbedfb4a5bbb
Nielsveldkamp <Nveldkamp1@st.noorderpoort.nl>
2023-03-29 09:42:32 +0200

    added bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,12 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+      integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
+      crossorigin="anonymous"
+    />
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
@@ -21,7 +26,8 @@
     .min-h-eightyPscreen{
         min-height: 80vh;
     }
-</style>
+    </style>
+    @stack('head')
 </head>
 
 <body class="font-sans antialiased">
