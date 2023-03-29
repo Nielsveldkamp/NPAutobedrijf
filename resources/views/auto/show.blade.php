4128abd5a0f017725f6466375f534e342e2ecf4f
Nielsveldkamp <Nveldkamp1@st.noorderpoort.nl>
2023-03-29 15:26:07 +0200

    css fix

--- resources/views/auto/show.blade.php
+++ resources/views/auto/show.blade.php
@@ -21,7 +21,7 @@
     
 </style>
 <x-app-layout>
-    <div class="py-12">
+    <div class="">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class=" overflow-hidden sm:rounded-lg">
                 <div class="p-6">
